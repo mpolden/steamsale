aca888b1e324e3902dc6b8f835655033e791fd5c
Martin Polden <martin.polden@gmail.com>
2011-12-23 21:35:37 +0100

    Convert to class

--- steamsale.py
+++ steamsale.py
@@ -1,62 +1,72 @@
 #!/usr/bin/env python
 
+""" A script that parses your Steam wishlist and finds discounts """
+
 import sys
 import requests
 from BeautifulSoup import BeautifulSoup
 
+class Wishlist(object):
+    def __init__(self, steam_id):
+        url = 'http://steamcommunity.com/profiles/%s/wishlist' % steam_id
+        req = requests.get(url)
+        self.soup = BeautifulSoup(req.content, 
+                convertEntities=BeautifulSoup.HTML_ENTITIES)
+        self.tag = None
+        self.items = []
+
+    def _find_price(self):
+        price = self.tag.find(attrs = {'class': 'price'})
+        return price.text if price and price.text else None
+
+    def _find_discount_pct(self):
+        discount_pct = self.tag.find(attrs = {'class': 'discount_pct'})
+        return discount_pct.text if discount_pct else None
+
+    def _find_org_price(self):
+        org_price = self.tag.find(attrs = {'class': 'discount_original_price'})
+        return org_price.text if org_price else None
+
+    def _find_final_price(self):
+        final_price = self.tag.find(attrs = {'class': 'discount_final_price'})
+        return final_price.text if final_price else None
+
+    def find_items(self):
+        """ Parse and find wishlist items """
+        # Find divs containing wishlist items
+        item_tags = self.soup.findAll(attrs = {'class': "wishlistRowItem\t"})
+        for item_tag in item_tags:
+            title = item_tag.find('h4').text
+
+            # Find div containing price data
+            self.tag = item_tag.find(attrs = {'class': 'gameListPriceData'})
+            default_price = self._find_price()
+            discount_pct = self._find_discount_pct()
+            original_price = self._find_org_price()
+            final_price = self._find_final_price()
+
+            self.items.append({
+                'title': title,
+                'discount_pct': discount_pct,
+                'original_price': original_price,
+                'final_price': default_price or final_price
+                })
+
+        return self.items
+
 def usage():
+    """ Display usage """
     print 'usage: %s steam_id' % sys.argv[0]
 
-def find_items(steam_id):
-    wishlist_url = 'http://steamcommunity.com/profiles/%s/wishlist' % steam_id
-
-    r = requests.get(wishlist_url)
-    soup = BeautifulSoup(r.content, 
-            convertEntities=BeautifulSoup.HTML_ENTITIES)
-    items = []
-
-    # Find divs containing wishlist items
-    item_tags = soup.findAll(attrs = {'class': "wishlistRowItem\t"})
-    for item_tag in item_tags:
-        title = item_tag.find('h4').text
-
-        # Find div containing price data
-        data_tag = item_tag.find(attrs = {'class': 'gameListPriceData'})
-
-        # Find default price
-        p_tag = data_tag.find(attrs = {'class': 'price'})
-        default_price = p_tag.text if p_tag and p_tag.text else None
-       
-        # Find discount percent if any
-        dp_tag = data_tag.find(attrs = {'class': 'discount_pct'})
-        discount_pct = dp_tag.text if dp_tag else None
-
-        # Original price    
-        dop_tag = data_tag.find(attrs = {'class': 'discount_original_price'})
-        original_price = dop_tag.text if dop_tag else None
-
-        # Final price
-        dfp_tag = data_tag.find(attrs = {'class': 'discount_final_price'})
-        discount_price = dfp_tag.text if dfp_tag else None
-
-        items.append({
-            'title': title,
-            'discount_pct': discount_pct,
-            'original_price': original_price,
-            'final_price': default_price or discount_price
-            })
-
-    return items
-
 def main():
-
+    """ Parse argv, find items and print them to stdout """
     if len(sys.argv) != 2:
         usage()
         return
-
+    
     steam_id = sys.argv[1]
-
-    items = find_items(steam_id)
+    wishlist = Wishlist(steam_id)
+    items = wishlist.find_items()
     for item in items:
         print '%s - price: %s' % (item['title'], item['final_price'])
 
